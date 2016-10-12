47303d6d3a8e9f13fd92e6052ea020159f798993
Shane Stephens <shans@chromium.org>
2016-10-12 22:05:12 +1100

    Simple caching proxy for issues

---
+++ api.py
@@ -0,0 +1,58 @@
+import webapp2
+import json
+from google.appengine.api import urlfetch
+from google.appengine.ext import ndb
+
+class Issue(ndb.Model):
+  json = ndb.TextProperty()
+
+class Patch(ndb.Model):
+  json = ndb.TextProperty()
+
+class ApiProxy(webapp2.RequestHandler):
+  def get(self, issueNumber):
+    self.response.headers.add_header("Access-Control-Allow-Origin", "*")
+
+    issue = Issue.get_by_id(issueNumber)
+    if issue is None:
+      result = urlfetch.fetch(url='http://codereview.chromium.org/api/' + issueNumber + '?messages=True').content
+      parsedResult = self.attachPatchsets(issueNumber, json.loads(result))
+      result = json.dumps(parsedResult)
+      if parsedResult['closed']:
+        issue = Issue(id=issueNumber, json=result)
+        issue.put()
+    else:
+      result = issue.json
+
+    if issue is not None:
+      self.response.headers['cache-control'] = 'max-age=315569000' 
+
+    self.response.write(result)
+
+  def attachPatchsets(self, issueNumber, parsedResult):
+    parsedResult['patchset_data'] = [];
+    rpcs = []
+    for patch in parsedResult['patchsets']:
+      rpc = urlfetch.create_rpc()
+      rpc.callback = (lambda rpc: (lambda: parsedResult['patchset_data'].append(rpc.get_result().content)))(rpc)
+      urlfetch.make_fetch_call(rpc, 'http://codereview.chromium.org/api/' + issueNumber + '/' + str(patch))
+      rpcs.append(rpc)
+    
+    for rpc in rpcs:
+      rpc.wait()
+    return parsedResult
+
+class SearchProxy(webapp2.RequestHandler):
+  def get(self):
+    print self.request.get('keys_only')
+    if not (self.request.get('keys_only') == "True"):
+      self.response.set_status(400)
+      return
+    print self.request.query_string
+    result = urlfetch.fetch(url='http://codereview.chromium.org/search?' + self.request.query_string).content
+    self.response.write(result)
+
+app = webapp2.WSGIApplication([
+  webapp2.Route(r'/api/<issueNumber>', handler=ApiProxy),
+  webapp2.Route(r'/search', handler=SearchProxy),
+], debug=True)
